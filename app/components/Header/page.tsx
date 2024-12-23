d19081446d9ae1847483c249fd04e3118bd85bed
siniseong <shinhuiseong07@gmail.com>
2024-12-23 12:26:13 +0900

    ADD :: 헤더 아래 라인 추가

--- app/components/Header/page.tsx
+++ app/components/Header/page.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Header: React.FC = () => {
   return (
-    <header className="flex items-center justify-between p-4 bg-white shadow-none"> 
+    <header className="flex items-center justify-between p-4 bg-white shadow-none border-b border-[#EBEBEB]"> {/* 아래 선 추가 */}
       <div className="flex items-center ml-[150px] mt-[10px]"> 
         <h1 className="text-2xl font-bold text-blue-600">바로우산</h1>
       </div>
