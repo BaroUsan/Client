a7cdeee4cfa8cce866c71cb31ef8e6e502d648d4
siniseong <shinhuiseong07@gmail.com>
2024-12-23 13:37:26 +0900

    FIX :: 헤더 로고 및 글자 크기 변경

--- app/components/Header/page.tsx
+++ app/components/Header/page.tsx
@@ -2,15 +2,15 @@ import React from 'react';
 
 const Header: React.FC = () => {
   return (
-    <header className="flex items-center justify-between p-4 bg-white shadow-none border-b border-[#EBEBEB]"> {/* 아래 선 추가 */}
+    <header className="flex items-center justify-between p-4 bg-white shadow-none border-b border-[#EBEBEB]"> 
       <div className="flex items-center ml-[150px] mt-[10px]"> 
-        <h1 className="text-2xl font-bold text-blue-600">바로우산</h1>
+        <img src="/logo.svg" alt="바로우산 로고" className="h-8" /> 
       </div>
       <nav className="flex space-x-14 mr-[150px]">
-        <a href="#" className="text-black hover:text-blue-600">홈</a>
-        <a href="#" className="text-black hover:text-blue-600">챗봇 대화</a>
-        <a href="#" className="text-black hover:text-blue-600">마이페이지</a>
-        <a href="#" className="text-black hover:text-blue-600">로그인</a>
+        <a href="/" className="text-lg text-black hover:text-blue-600">홈</a> 
+        <a href="#" className="text-lg text-black hover:text-blue-600">챗봇 대화</a> 
+        <a href="#" className="text-lg text-black hover:text-blue-600">마이페이지</a> 
+        <a href="#" className="text-lg text-black hover:text-blue-600">로그인</a>
       </nav>
     </header>
   );
